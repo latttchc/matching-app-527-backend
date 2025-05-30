f0771ab4b4c54cc6521f3ed3c4c8e12ac96d0a77
latttchc <iam.yuk2882@gmail.com>
2025-05-30 15:13:55 +0900

    feat: alias current_api_v1_user as current_user for consistent access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
+      
+        alias_method :current_user, :current_api_v1_user
 end
+      
