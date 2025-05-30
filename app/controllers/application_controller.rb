b303840ec6ad84a782e8c4d860ecc95058a0d708
latttchc <iam.yuk2882@gmail.com>
2025-05-31 00:31:43 +0900

    fix: testAPIからのレスポンスエラー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
-      
+
         alias_method :current_user, :current_api_v1_user
+
+        before_action :set_cors_headers
+
+        private
+
+        def set_cors_headers
+            response.headers["Access-Control-Allow-Credentials"] = "true"
+        end
 end
-      
