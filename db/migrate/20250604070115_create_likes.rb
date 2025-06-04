56543a5ce6ba8f37e3f5b37948bf94f1c081ca82
latttchc <iam.yuk2882@gmail.com>
2025-06-04 16:12:33 +0900

    add: Likeモデルにカラム追加をしてmigrate

--- db/migrate/20250604070115_create_likes.rb
+++ db/migrate/20250604070115_create_likes.rb
@@ -1,6 +1,9 @@
 class CreateLikes < ActiveRecord::Migration[8.0]
   def change
     create_table :likes do |t|
+      t.integer :form_user_id, null: false #誰が
+      t.integer :to_user_id, null: false #誰に
+
       t.timestamps
     end
   end
