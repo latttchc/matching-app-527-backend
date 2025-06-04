fafcac39e98a78f65535857bb0a5016f7055ac4d
latttchc <iam.yuk2882@gmail.com>
2025-06-04 17:21:05 +0900

    feat: migrate Message_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_04_074331) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_04_081727) do
   create_table "chat_room_users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "chat_rooms_id", null: false
     t.integer "user_id", null: false
@@ -26,6 +26,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_04_074331) do
   create_table "likes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "from_user_id", null: false
     t.integer "to_user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "chat_room_id", null: false
+    t.integer "user_id", null: false
+    t.string "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
